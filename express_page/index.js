5ae38ab87d79260feb160a72b0e5acfe01581be4
Adiguna <adigunarizqi@gmail.com>
2023-06-13 08:26:26 +0700

    tambah blacky di cats

--- express_page/index.js
+++ express_page/index.js
@@ -32,7 +32,7 @@ app.get(`/t/:tag`, (req, res) => {
 });
 
 app.get(`/cats`, (req, res) => {
-  const cats = [`jack`, `back`, `kek`, `john`, `jake`];
+  const cats = [`jack`, `back`, `kek`, `john`, `jake`, `blacky`];
   res.render(`cats`, { cats });
 });
 
